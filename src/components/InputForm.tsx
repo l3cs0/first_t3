fb50e47d79800c82bb247f64b238047f207aea62
leon <l.sottru@web.de>
2023-09-12 12:04:37 +0200

    d

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,5 +1,5 @@
-import { NumberInput } from "@mui/base/Unstable_NumberInput/NumberInput";
 import { type ChangeEvent, useState } from "react";
+import { Unstable_NumberInput as NumberInput } from "@mui/base";
 // import { set } from "zod";
 // import { api } from "~/utils/api";
 
@@ -13,7 +13,7 @@ export const InputForm = () => {
   const [price, setPrice] = useState(0);
   const [tip, setTip] = useState(0);
 
-  const setPriceAndTip = (price?: number, tip?: number) => {
+  const setPriceAndTip = (price?: any, tip?: any) => {
     if (typeof price === "number" && price >= 0) {
       setPrice(price);
     }
@@ -24,13 +24,10 @@ export const InputForm = () => {
   return (
     <div className="grid grid-cols-3 gap-1 pb-4">
       <NumberInput
-        className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle
-        text-white sm:max-w-xs"
         tabIndex={1}
         onChange={(e, value) => setPriceAndTip(value)}
       />
       <NumberInput
-        className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle text-white sm:max-w-xs"
         tabIndex={2}
         placeholder="Trinkgeld"
         onChange={(e, value) => setPriceAndTip(undefined, value)}
