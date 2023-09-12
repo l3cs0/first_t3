25622e293b7fb63271c6a1e8ff97cfdf6741c84a
leon <l.sottru@web.de>
2023-09-12 14:25:15 +0200

    change to standard input

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,42 +1,47 @@
-import { type ChangeEvent, useState } from "react";
-import { Unstable_NumberInput as NumberInput } from "@mui/base";
-// import { set } from "zod";
-// import { api } from "~/utils/api";
+import { useState } from "react";
 
 export const InputForm = () => {
-  const handleNewEntry = (price: number, tip: number) => {
+  const [price, setPrice] = useState("");
+  const [tip, setTip] = useState("");
+
+  const handleNewEntry = (p: string, t: string) => {
+    console.log(`price: ${p}, tip: ${t}`);
     // api.entries.addEntry.useMutation().mutate({ price: price, tip: tip });
-
-    setPriceAndTip(0, 0);
   };
 
-  const [price, setPrice] = useState(0);
-  const [tip, setTip] = useState(0);
-
-  const setPriceAndTip = (price?: any, tip?: any) => {
-    if (typeof price === "number" && price >= 0) {
-      setPrice(price);
-    }
-    if (typeof tip === "number" && tip >= 0) {
-      setTip(tip);
-    }
-  };
   return (
     <div className="grid grid-cols-3 gap-1 pb-4">
-      <NumberInput
+      <input
+        className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle text-white sm:max-w-xs"
+        autoFocus
+        type="text"
+        inputMode="numeric"
         tabIndex={1}
-        onChange={(e, value) => setPriceAndTip(value)}
+        value={price}
+        pattern="\d+(.\d{1})?"
+        placeholder="Preis"
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
       />
-      <NumberInput
-        tabIndex={2}
+      <input
+        className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle text-white sm:max-w-xs"
+        autoFocus
+        value={tip}
+        type="text"
+        inputMode="numeric"
+        tabIndex={1}
+        pattern="\d+(.\d{1})?"
         placeholder="Trinkgeld"
-        onChange={(e, value) => setPriceAndTip(undefined, value)}
+        onChange={(e) => {
+          setTip(e.target.value);
+        }}
       />
       <button
         type="button"
         className="rounded-lg bg-yellow-500 px-1 py-1 text-center align-middle text-black sm:max-w-xs"
         onClick={() => {
-          handleNewEntry(1, 0);
+          handleNewEntry(price, tip);
         }}
       >
         Speichern
