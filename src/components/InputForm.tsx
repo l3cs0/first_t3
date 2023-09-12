77e75b29cda9d06c5c048bb34dd9ecda49acbb81
leon <l.sottru@web.de>
2023-09-12 11:54:49 +0200

    commit

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,28 +1,39 @@
-import { api } from "~/utils/api";
+import { NumberInput } from "@mui/base/Unstable_NumberInput/NumberInput";
+import { type ChangeEvent, useState } from "react";
+// import { set } from "zod";
+// import { api } from "~/utils/api";
 
 export const InputForm = () => {
   const handleNewEntry = (price: number, tip: number) => {
-    api.entries.addEntry.useMutation().mutate({ price: price, tip: tip });
+    // api.entries.addEntry.useMutation().mutate({ price: price, tip: tip });
+
+    setPriceAndTip(0, 0);
+  };
+
+  const [price, setPrice] = useState(0);
+  const [tip, setTip] = useState(0);
+
+  const setPriceAndTip = (price?: number, tip?: number) => {
+    if (typeof price === "number" && price >= 0) {
+      setPrice(price);
+    }
+    if (typeof tip === "number" && tip >= 0) {
+      setTip(tip);
+    }
   };
   return (
     <div className="grid grid-cols-3 gap-1 pb-4">
-      <input
-        className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle text-white sm:max-w-xs"
-        autoFocus
-        type="text"
-        inputMode="numeric"
+      <NumberInput
+        className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle
+        text-white sm:max-w-xs"
         tabIndex={1}
-        value=""
-        pattern="\d+(.\d{1})?"
-        placeholder="Preis"
+        onChange={(e, value) => setPriceAndTip(value)}
       />
-      <input
+      <NumberInput
         className="rounded-lg bg-black/60 px-1 py-1 text-center align-middle text-white sm:max-w-xs"
-        type="text"
-        inputMode="numeric"
         tabIndex={2}
-        pattern="\d+(.\d{1})?"
         placeholder="Trinkgeld"
+        onChange={(e, value) => setPriceAndTip(undefined, value)}
       />
       <button
         type="button"
